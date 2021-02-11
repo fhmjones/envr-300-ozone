e9a16f37e7ea18984e6a23e85343364bb2ce21ef
fhmjones <fjones@eoas.ubc.ca>
2021-02-11 15:11:07 -0800

    fix formating of question set

--- app.py
+++ app.py
@@ -155,7 +155,9 @@ app.layout = html.Div([
 # then the text for instruction or to define the assignment.
     dcc.Markdown('''
         ## Usage for teaching and learning
+
         Here are a few examples of questions that could be posed in class, on your own, or as an assignment. Choices will depend on whether this dashboard is being used to compare ozone levels at a coastal location to those further inland, or whether the purpose is to explore the challenges and potential for working with "messy" data sets. 
+        
         1. Plot just one raw data set. How much variation is there over the whole year? 
         2. Use zoom and scrolling functionality to estimate daily variability of this parameter. 
         3. What times during the year seem to have lowest ozone levels? Highest Ozone levels? How difficult is it to make these judgements 
@@ -164,8 +166,10 @@ app.layout = html.Div([
         6. Which of these two processing options makes it easier (or more effective) to compare these two stations stations? Why? 
         7. Look closely at a day or two of smoothed and mda8 data. You should see they appear to be not quite "lined up". Why is this? _{{Because smoothed values are hourly wherease mda8 is a daily value assigned to the date at "0" hours. So the mda8 peak may not match up with a smoothed hourly peak. Most daily peaks are in the afternoon, especially in the summer.}}_
         8. many other ideas ...
+        
         ---
         ## Attribution
+        
         * Data used here are hourly ozone (parts per billion) for 2017 only, from 2 of many monitoring stations. Full datasets can be found at the BC Data Catalogue, [Air Quality Monitoring: Verified Hourly Data](https://catalogue.data.gov.bc.ca/dataset/77eeadf4-0c19-48bf-a47a-fa9eef01f409), licensed under the [Open Government Licence – British Columbia](https://www2.gov.bc.ca/gov/content/data/open-data/open-government-licence-bc). For more information about these data and their source, see the [Status of Ground-Level Ozone in B.C. (2015-2017)](http://www.env.gov.bc.ca/soe/indicators/air/ozone.html) web page.
         * The idea is derived from a discussion between Tara Ivanochko and Rivkah Gardner-Frolick <rivkahgf@gmail.com> who uses the complete dataset as part of a [Python tutorial](https://colab.research.google.com/drive/1DO0ICvInsr74vnl3AcPBoGtJyNrV-J8F?usp=sharing#scrollTo=a5l7UD_njHPv) on importing modules, importing data, plotting timeseries and scatter plots.
         * Code by [Francis Jones](https://www.eoas.ubc.ca/people/francisjones).  
